60859a1909497f989ea11d7027c6907632237f4d
Carry <carryaway91@gmail.com>
2023-03-05 08:37:22 +0100

    added working position

--- src/containers/about/About.js
+++ src/containers/about/About.js
@@ -11,10 +11,11 @@ const About = () => {
         <h3>About</h3>
         <p>
           My name is Peter Babej-Kmec, and I am currently located in Kosice,
-          Slovakia. My first encounter with programming was when I was studying
-          translation and interpretation at the university. It was love at first
-          sight. The very idea that you can create something and put it on the
-          internet for anyone to see and use was fascinating to me.
+          Slovakia where I work as a UI designer. My first encounter with
+          programming was when I was studying translation and interpretation at
+          the university. It was love at first sight. The very idea that you can
+          create something and put it on the internet for anyone to see and use
+          was fascinating to me.
         </p>
         <p>
           Right away I started to play around with web design and was searching
